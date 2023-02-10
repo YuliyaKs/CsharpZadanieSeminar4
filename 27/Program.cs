b9c9a589c91a0915f51696286c49c939b5971146
Yuliya_K <live2do@yandex.ru>
2023-02-10 16:41:47 +0300

    add Задача 27. Сумма цифр

---
+++ 27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {NumbersSum(number)}");
+int NumbersSum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num /=10;
+    }
+    return result;
+}
