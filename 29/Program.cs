c454b8c31ee0242316ff12a96e199ddba4f4e083
Yuliya_K <live2do@yandex.ru>
2023-02-10 17:12:03 +0300

    add Задача 29. Вывод массива

---
+++ 29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+Console.Write("Введите начало диапазона: ");
+int begin = int.Parse(Console.ReadLine());
+Console.Write("Введите конец диапазона: ");
+int end = int.Parse(Console.ReadLine());
+Console.Write($"[{String.Join(", ", PrintArray(begin, end))}]");
+int[] PrintArray(int start, int finish)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(start, finish);
+    }
+    return array;
+}
